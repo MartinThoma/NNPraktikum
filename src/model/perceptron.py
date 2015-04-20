32464f3d504c8fb295dd9f99b2e9d24012609f67
Martin Thoma <info@martin-thoma.de>
2015-04-20 15:33:31 +0200

    model/perceptron: implemented perceptron training rule

--- src/model/perceptron.py
+++ src/model/perceptron.py
@@ -2,6 +2,15 @@ from model.classifier import Classifier
 import numpy as np
 from util.activation_functions import Activation
 
+from sklearn.metrics import accuracy_score
+
+import logging
+import sys
+
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
+                    level=logging.DEBUG,
+                    stream=sys.stdout)
+
 
 class Perceptron(Classifier):
     """
@@ -37,11 +46,23 @@ class Perceptron(Classifier):
         # around 0 and0.1
         self.weight = np.random.rand(self.trainingSet.input.shape[1], 1)/1000
 
-    def train(self):
-        """Train the perceptron with the perceptron learning algorithm."""
-        # TODO: Here you have to implement the Perceptron Learning Algorithm
-        # TODO: use self.trainingSet
-        # TODO: use self.validationSet
+    def train(self, verbose=True):
+        """Train the perceptron with the perceptron learning algorithm.
+
+        Parameters
+        ----------
+        verbose : bool
+            Print logging messages with validation accuracy if verbose is True.
+        """
+        for i in range(1, self.epochs+1):
+            pred = self.evaluate(self.validationSet.input)
+            if verbose:
+                val_acc = accuracy_score(self.validationSet.label, pred)*100
+                logging.info("Epoch: %i (Validation acc: %0.4f%%)", i, val_acc)
+            for X, y in zip(self.trainingSet, self.trainingSet.label):
+                pred = self.classify(X)
+                X = np.array([X]).reshape(784, 1)
+                self.weight += self.learningRate * (y - pred) * X * (-1)
 
     def classify(self, testInstance):
         """Classify a single instance.
@@ -55,13 +76,14 @@ class Perceptron(Classifier):
         bool :
             True if the testInstance is recognized as a 7, False otherwise.
         """
-        # TODO: Here you have to implement the classification for one instance
         return self.fire(testInstance)
 
-    def evaluate(self):
+    def evaluate(self, data=None):
+        if data is None:
+            data = self.testSet.input
         # One you can classify an instance, just use map for all of the test
         # set.
-        return list(map(self.classify, self.testSet.input))
+        return list(map(self.classify, data))
 
     def fire(self, input):
         return Activation.sign(np.dot(np.array(input), self.weight))
